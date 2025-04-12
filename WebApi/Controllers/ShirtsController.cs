96cfa7abc1141f9e261116dba0cf533b9d0b8d81
WedithaV <wedithav@gmail.com>
2025-04-12 10:53:59 +0530

    Create controller based webapi method 1

---
+++ WebApi/Controllers/ShirtsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+
+    public class ShirtsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/shirts")]
+        public string GetShirts()
+        {
+            return "Read all shirts";
+        }
+
+        [HttpGet]
+        [Route("/shirts/{id}")]
+        public string GetShirtById(int id)
+        {
+            return $"Read shirt {id}";
+        }
+
+        [HttpPost]
+        [Route("/shirts")]
+        public string PostShirt()
+        {
+            return "Created a shirt";
+        }
+
+        [HttpDelete]
+        [Route("/shirts/{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"Deleted shirt {id}";
+        }
+
+        [HttpPut]
+        [Route("/shirts/{id}")]
+        public string PutShirt(int id) {
+            return $"Updated shirt {id}";
+        }
+    }
+}
+

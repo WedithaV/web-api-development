a3d1a11c2876ca542e89e381a1df6672522d1b2b
WedithaV <wedithav@gmail.com>
2025-04-12 21:32:06 +0530

    Web api return types

--- WebApi/Controllers/ShirtsController.cs
+++ WebApi/Controllers/ShirtsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Drawing;
+using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -8,6 +9,13 @@ namespace WebApi.Controllers
 
     public class ShirtsController : ControllerBase
     {
+        private List<Shirt> shirts = new List<Shirt>() {
+            new Shirt {ShirtId = 1, Brand = "My Brand", Color = "black",Gender = "Men",Prize = 34.23, Size = 9},
+            new Shirt {ShirtId = 2, Brand = "My Brand", Color = "black",Gender = "Men",Prize = 34.23, Size = 9},
+            new Shirt {ShirtId = 3, Brand = "My Brand", Color = "black",Gender = "Men",Prize = 34.23, Size = 9},
+            new Shirt {ShirtId = 4, Brand = "My Brand", Color = "black",Gender = "Men",Prize = 34.23, Size = 9},
+        };
+
         [HttpGet]
         public string GetShirts()
         {
@@ -15,9 +23,19 @@ namespace WebApi.Controllers
         }
 
         [HttpGet("{id}")]
-        public string GetShirtById(int id)
+        public IActionResult GetShirtById(int id)
         {
-            return $"Read shirt {id}";
+            if(id < 0)
+            {
+                return BadRequest();
+            }
+
+            var shirt = shirts.FirstOrDefault(x => x.ShirtId == id);
+            if(shirt == null)
+            {
+                return NotFound();
+            }
+            return Ok(shirt);
         }
 
         [HttpPost]
